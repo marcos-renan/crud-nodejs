336b42bad2d23a4ed2b5f24603003f01f234de50
Marcos <devmarcos7@gmail.com>
2025-03-20 20:46:08 -0300

    realizando conexao com o BD

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-// importando express
+//importando express
 import express from 'express';
+//importando conector do BD
+import mysql from 'mysql2';
 
 //criando varivel app
 const app = express();
+
+//conexao com o BD
+const conexao = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'admin',
+    database:'crudnode'
+});
+
+//teste de conexao
+conexao.connect(function(erro){
+    if(erro) throw erro;
+    console.log('Conexao bem sucedida!');
+})
 
 //rota hello world do tipo get
 app.get('/', function(req, res){
